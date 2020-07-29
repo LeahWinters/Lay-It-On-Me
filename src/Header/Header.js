2070ab7cbf40f7ecf3283390384a50329928279b
Leah Winters <leah.winters24@gmail.com>
2020-07-29 13:12:04 -0600

    Create Header component with title and two buttons

---
+++ src/Header/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <Link to="/">
+        <h1 className="title">Lay It On Me</h1>
+      </Link>
+     
+      <Link to="/random">
+        <button 
+          className="random-btn"
+          type="button"
+          aria-label="random button"
+        >
+          Random Advice
+        </button>
+      </Link>
+      <Link to="/my_saved_advice">
+        <button 
+          className="saved-advice-btn"
+          type="button"
+          aria-label="saved advice button"
+        >
+          Saved Advice
+        </button>
+      </Link>
+    </div>
+  )
+}
+
+export default Header;
