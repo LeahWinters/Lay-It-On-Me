0155bf4a8e12da46789e5bb35c22a11e0add292c
Leah Winters <leah.winters24@gmail.com>
2020-07-29 14:17:23 -0600

    Add test to ensure header is displayed on the dom with all the correct elements

--- src/Header/Header.test.js
+++ src/Header/Header.test.js
@@ -1,5 +1,26 @@
 import React from 'react';
 import Header from './Header';
 import '@testing-library/jest-dom';
-import { render, fireEvent, waitFor, getByLabelText } from '@testing-library/react';
+// import ReactDOM from "react-dom";
+// import "@testing-library/jest-dom/extend-expect";
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+
+describe("Header", () => {
+  it('Should display the title of the application and two buttons', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    );
+
+    const title = getByText("Lay It On Me");
+    const randomBtn = getByText("Random Advice");
+    const savedBtn = getByText("Saved Advice");
+
+    expect(title).toBeInTheDocument();
+    expect(randomBtn).toBeInTheDocument();
+    expect(savedBtn).toBeInTheDocument();
+
+  });
+})
