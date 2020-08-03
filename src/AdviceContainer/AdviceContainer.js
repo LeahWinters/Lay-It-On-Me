1bccf02ee099233d9a26e6422e454668e9f5fda4
Leah Winters <leah.winters24@gmail.com>
2020-08-03 15:46:50 -0600

    Remove an import of Link where it was not needed

--- src/AdviceContainer/AdviceContainer.js
+++ src/AdviceContainer/AdviceContainer.js
@@ -5,8 +5,8 @@ import AdviceCard from "../AdviceCard/AdviceCard";
 const AdviceContainer = ({ searchedAdvice, saveAdvice }) => {
   const arrayToDisplay = searchedAdvice.map((slip) => {
     return (
-      <AdviceCard 
-        advice={slip.advice} 
+      <AdviceCard
+        advice={slip.advice}
         id={slip.id}
         key={slip.id}
         saveAdvice={saveAdvice}
@@ -15,11 +15,7 @@ const AdviceContainer = ({ searchedAdvice, saveAdvice }) => {
     );
   });
 
-  return (
-    <section className="advice-container">
-      {arrayToDisplay}
-    </section>
-  );
+  return <section className="advice-container">{arrayToDisplay}</section>;
 };
 
 export default AdviceContainer;
